32a24a0599fbb5451db26a3d6cda707c47c734e7
Sebastian Slight <williamcortina49@gmail.com>
2026-03-12 04:25:15 -0700

    Add clawstrider/utils/logging.py

---
+++ clawstrider/utils/logging.py
@@ -0,0 +1,12 @@
+import logging
+
+def setup_logger(name: str):
+    logger = logging.getLogger(name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
